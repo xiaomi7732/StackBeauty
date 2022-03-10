7f87c0a2b5e4685423a90f13c04881e8f5fbcf18
Saar Shen <xiaomi7732@hotmail.com>
2022-03-09 22:35:00 -0800

    Basic functionality works

--- src/NetStackBeautifier.VSCExt/src/extension.ts
+++ src/NetStackBeautifier.VSCExt/src/extension.ts
@@ -2,23 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import axios from 'axios';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "stackbeauty" is now active!');
-
     let showDateTimeCommand = vscode.commands.registerCommand('stackbeauty.showDateTime', () => {
-
-        axios({
-            'method': 'get',
-            'url': 'https://stackbeauty-dev.whiteplant-313c6159.eastus2.azurecontainerapps.io/healthz',
-        }).then(response => {
-            console.log(response.data);
-        });
         vscode.window.showWarningMessage('Current datetime: ' + (new Date()).toISOString() + 'in theme: ' + vscode.window.activeColorTheme.kind);
         vscode.debug.activeDebugConsole.appendLine("Hello debug info");
     });
@@ -31,8 +18,106 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage('Hello from StackBeauty!');
     });
 
+    let showBeautified = vscode.commands.registerCommand('stackbeauty.showBeautified', () => {
+        createPanel(context);
+    });
+
     context.subscriptions.push(disposable);
     context.subscriptions.push(showDateTimeCommand);
+    context.subscriptions.push(showBeautified);
+}
+
+function createPanel(context: vscode.ExtensionContext) {
+    console.log('Show Beautified is called.');
+    if (vscode.window.activeTextEditor === undefined) {
+        vscode.window.showErrorMessage("No content to beautify. No active text editor is detected.");
+        return;
+    }
+
+    let selectedText = vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection);
+    if (selectedText === '') {
+        console.log('No text selected');
+        selectedText = vscode.window.activeTextEditor.document.getText();
+    }
+    console.log(`selected text: ${selectedText}`);
+
+    if (selectedText === undefined || selectedText === '') {
+        vscode.window.showErrorMessage("No text is available for beautifying.");
+        return;
+    }
+
+    const panel = vscode.window.createWebviewPanel(
+        'stackbeauty',
+        'Beautified Stack',
+        vscode.ViewColumn.Two,
+        {
+            // Enable javascript in the webview
+            enableScripts: true,
+        }
+    );
+
+    getHtmlForWebview(context, selectedText).then((result: string) => {
+        panel.webview.html = result;
+    }).catch(err => {
+        vscode.window.showErrorMessage("Unexpected error " + err);
+        return;
+    });
+}
+
+async function getHtmlForWebview(context: vscode.ExtensionContext, inputText: string): Promise<string> {
+    const backend: string = "https://stackbeauty-dev.whiteplant-313c6159.eastus2.azurecontainerapps.io";
+    const healthzEndpoint: string = "/healthz";
+    const beautifiedEndpoint: string = "/Beautified";
+    const htmlEndpoint: string = "/Html";
+
+    const healthCheckResponse = await axios({
+        'method': 'get',
+        'baseURL': backend,
+        'url': healthzEndpoint,
+    });
+
+    if (!healthCheckResponse || healthCheckResponse.status !== 200) {
+        vscode.window.showErrorMessage("The backend for the Beautifier can't be reached...");
+        return "";
+    }
+
+    const stackJson = await axios({
+        'method': 'post',
+        'url': beautifiedEndpoint,
+        'baseURL': backend,
+        'headers': { 'Content-Type': 'text/plain' },
+        'data': inputText,
+    });
+    console.log('Got json back:' + JSON.stringify(stackJson.data));
+
+    if (!stackJson || stackJson.status !== 200 || !stackJson.data || stackJson.data.length === 0) {
+        console.log('No beautify result');
+        return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+        </head>
+        <body>
+            There's no beautify result. Is the selection a valid callstack?
+        </body>
+        </html>`;
+    }
+
+    const htmlResponse = await axios({
+        'method': 'post',
+        'baseURL': backend,
+        'url': htmlEndpoint,
+        'headers': { 'Content-Type': 'application/json' },
+        'data': stackJson.data,
+    });
+
+    if (!htmlResponse || htmlResponse.status !== 200) {
+        console.log('Not able to get html for beautified content.');
+    }
+    return htmlResponse.data;
 }
 
 // this method is called when your extension is deactivated
